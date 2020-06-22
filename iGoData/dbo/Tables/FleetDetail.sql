95678fec4ac669554278fc4f9b9dfe53e871a6a3
OnSight Office <OnSight Office@SAKIS-ONSIGHT>
2020-06-22 19:49:57 +0300

    allages db

---
+++ iGoData/dbo/Tables/FleetDetail.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[FleetDetail]
+(
+	[Vehicle Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[Vehicle Name]                    NCHAR (10) NOT NULL,
+    [Type]                            NCHAR (10) NULL,
+    [Transmision]                     NCHAR (10) NULL,
+    [Model]                           SMALLINT   NULL,
+    [Color]                           NCHAR (10) NULL,
+    [Seats]                           TINYINT    NULL,
+    [Doors]                           TINYINT    NULL,
+    [Fuel]                            NCHAR (10) NULL,
+    [Cubic Centimetres]               SMALLINT   NULL,
+    [Category]                        NCHAR (10) NULL,
+
+)
