44c19f9709402d8cc3e819586178aefb10322d4f
OnSight Office <OnSight Office@SAKIS-ONSIGHT>
2020-06-22 14:37:43 +0300

    igodata dbo

---
+++ iGoData/dbo/Tables/Customers.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [Customers Id]             INT          NOT NULL,
+    [First_name]     CHAR (10)    NOT NULL,
+    [Last_name]      CHAR (20)    NOT NULL,
+    [Age]            TINYINT      NULL,
+    [Company]        CHAR (20)    NULL,
+    [Address Line 1] CHAR (20)    NOT NULL,
+    [Address Line 2] CHAR (20)    NULL,
+    [City]           CHAR (20)    NOT NULL,
+    [Postcode]       INT          NOT NULL,
+    [Country]        CHAR (10)    NOT NULL,
+    [State]          CHAR (10)    NULL,
+    [E-mail]         VARCHAR (25) NOT NULL,
+    [Phone]          INT          NOT NULL,
+    PRIMARY KEY CLUSTERED ([Customers Id] ASC)
+);
